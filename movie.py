6930516ef64de4ccf7c8f74d2c3f7657f922ab0f
Akif Küçükbenli <akifkucukbenli@gmail.com>
2023-10-07 00:27:10 +0300

    movie_cut convert audiofile

--- movie.py
+++ movie.py
@@ -7,3 +7,7 @@ final_clip = input_video.subclip(0,11)
 final_clip.write_videofile("cut_video.mp4")
 
 print("your clip ready!")
+
+from moviepy.editor import VideoFileClip
+
+VideoFileClip("cut_video.mp4").audio.write_audiofile("cut_music.mp3")
